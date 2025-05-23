7bed220c3081c3abd330ec6e2741274f636ca14b
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-05-23 20:01:53 +0300

    Pridėta cross-reference lentelė

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,18 @@
 #include <cctype>
 #include <regex>
 
+bool has_forbidden_inside(const string& word, const std::set<char>& forbidden) {
+    if (word.size() <= 2) {
+        return false;
+    }
+    for (size_t i = 1; i + 1 < word.size(); ++i) {
+        if (forbidden.count(word[i]) > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     ifstream input("tekstas.txt");
     if (!input.is_open()) {
@@ -12,27 +24,46 @@ int main() {
     }
     ofstream output("rezultatai.txt");
 
+    std::set<char> forbidden_inside_chars = {'.', '/', '\\', '|', ':', '*', '+', '=', '@'};
     std::regex front_unwanted(R"(^[\"'„“°.,:;!?()\[\]{}<>]+)");
     std::regex back_unwanted(R"([\"'„“°.,:;!?()\[\]{}<>]+$)");
 
     std::map<string, int> wordCount;
+    std::map<string, std::set<int>> wordLines;
+
     string line;
+    int lineNumber = 1;
     while (getline(input, line)) {
         std::istringstream iss(line);
         string word;
         while (iss >> word) {
             word = std::regex_replace(word, front_unwanted, "");
             word = std::regex_replace(word, back_unwanted, "");
+            if (has_forbidden_inside(word, forbidden_inside_chars))
+                    continue;
             std::transform(word.begin(), word.end(), word.begin(), ::tolower);
             if (!word.empty() && word.length() > 1 && std::any_of(word.begin(), word.end(), ::isalpha))
                 wordCount[word]++;
+                wordLines[word].insert(lineNumber);
         }
+        ++lineNumber;
     }
 
     for (const auto& pair : wordCount) {
-        if (pair.second > 1)
-            output << pair.first << " : " << pair.second << endl;
+        if (pair.second > 1) {
+            output << left << setw(20) << pair.first;
+            output << " | " << setw(2) << pair.second << " | (";
+            auto it = wordLines[pair.first].begin();
+            while (it != wordLines[pair.first].end()) {
+                output << *it;
+                ++it;
+                if (it != wordLines[pair.first].end()) {
+                    output << ", ";
+                }
+            }
+        output << ")" << endl;
     }
+}
 
     cout << "Skaiciavimas baigtas. Rezultatai issaugoti faile rezultatai.txt" << endl;
     input.close();
